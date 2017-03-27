23d4de7fe48ada54735aac4dd75c1bcbcb6e75cc
Pavel Zubkou <pavel.zubkou@gmail.com>
2017-03-27 14:29:04 +0300

    Expose data from getData function

--- getData.js
+++ getData.js
@@ -1,3 +1,19 @@
+function handeResponse(response) {
+  if (response.status !== 200) {
+    throw new Error('Looks like there was a problem. Status Code: ' +
+      response.status);
+  }
+
+  // Examine the text in the response
+  return response.json().then(function(data) {
+    return data;
+  });
+}
+
 module.exports = function getData() {
-  console.log('getData called');
+  return fetch('https://httpbin.org/ip')
+    .then(handeResponse)
+    .catch(function(err) {
+      console.log('Fetch Error :-S', err);
+    });
 }
--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const getData = require('./getData');
 
-console.log('Hello World');
+console.log('Hello World, s13');
+
+const dataPromise = getData();
+
+dataPromise.then(function (data) {
+  console.log('then', data)
+})
+
+console.log('end');
